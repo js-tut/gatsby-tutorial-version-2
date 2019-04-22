a1651e29455064862c0fb58e5cbc44bd6a19da04
js-tut <johnsmilga@gmail.com>
2019-04-21 22:52:47 -0700

    inline styles and global styles

---
+++ src/components/banner.js
@@ -0,0 +1,5 @@
+import React from "react"
+import "./banner.css"
+export default function banner() {
+  return <div className="title">hello i'm awesome banner</div>
+}
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,11 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import Banner from "../components/banner"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1 className="title">Hi from the second page</h1>
+    <Banner />
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
